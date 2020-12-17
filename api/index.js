d55ae51a4a58108381dbf84e0628d850499c6116
deCha Sanson <deCha92@hotmail.com>
2020-12-16 20:29:40 -0600

    Started building out db index file

--- api/index.js
+++ api/index.js
@@ -1,34 +1,30 @@
 // Build an apiRouter using express Router
-
+const express = require("express");
+const apiRouter = express.Router();
 
 // Import the database adapter functions from the db
 
-
 /**
  * Set up a GET request for /reports
- * 
+ *
  * - it should use an async function
  * - it should await a call to getOpenReports
  * - on success, it should send back an object like { reports: theReports }
  * - on caught error, call next(error)
  */
 
-
-
 /**
  * Set up a POST request for /reports
- * 
+ *
  * - it should use an async function
  * - it should await a call to createReport, passing in the fields from req.body
  * - on success, it should send back the object returned by createReport
  * - on caught error, call next(error)
  */
 
-
-
 /**
  * Set up a DELETE request for /reports/:reportId
- * 
+ *
  * - it should use an async function
  * - it should await a call to closeReport, passing in the reportId from req.params
  *   and the password from req.body
@@ -36,18 +32,14 @@
  * - on caught error, call next(error)
  */
 
-
-
 /**
  * Set up a POST request for /reports/:reportId/comments
- * 
+ *
  * - it should use an async function
  * - it should await a call to createReportComment, passing in the reportId and
  *   the fields from req.body
  * - on success, it should send back the object returned by createReportComment
  * - on caught error, call next(error)
  */
-
-
 
 // Export the apiRouter
